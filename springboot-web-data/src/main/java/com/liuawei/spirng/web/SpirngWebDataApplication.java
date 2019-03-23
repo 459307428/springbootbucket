da9f9db9174862d31264529d03cbc002909d6677
lwj <lwj@lwj-PC>
2019-03-23 15:52:17 +0800

    Init

---
+++ springboot-web-data/src/main/java/com/liuawei/spirng/web/SpirngWebDataApplication.java
@@ -0,0 +1,13 @@
+package com.liuawei.spirng.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpirngWebDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpirngWebDataApplication.class, args);
+	}
+
+}
